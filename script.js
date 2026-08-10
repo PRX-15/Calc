4979fb03c68023e2a47abded290058bb74b842f7
PRX <prashx15@gmail.com>
2026-08-10 21:14:17 +0530

    Update script.js
    offline shi

--- script.js
+++ script.js
@@ -1195,3 +1195,15 @@ loadTheme();
 renderHistory();
 
 render();
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("./sw.js")
+            .catch(error => {
+                console.error(
+                    "Service Worker registration failed:",
+                    error
+                );
+            });
+    });
+}
